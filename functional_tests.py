0a890bcab7ce3e5f83a66b89191b0d7c20710382
upizzz <upinderawat@gmail.com>
2019-01-14 22:00:21 +0530

    Added Funtional Tests

---
+++ functional_tests.py
@@ -0,0 +1,19 @@
+from selenium import webdriver
+import unittest
+
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Chrome()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        self.browser.get('http://locahost:8000')
+        self.assertIn('To Do', self.browser.title)
+        self.fail('Finish the Test')
+
+if __name__ == '__main__':
+    unittest.main(
+        warnings='ignore'
+    )
